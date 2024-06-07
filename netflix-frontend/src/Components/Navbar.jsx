e94b4754c63fddf0d6c1993c14249181fb891729
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2024-06-08 00:23:08 +0530

    Updated Netflix.jsx home page.

--- netflix-frontend/src/Components/Navbar.jsx
+++ netflix-frontend/src/Components/Navbar.jsx
@@ -1,16 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { firebaseAuth } from "../Utils/firebase-config";
+import {onAuthStateChanged } from 'firebase/auth';
 
-
-export default function Navbar({isScrolled}){
+export default function Navbar(){
     const links = [
         { name: "Home", link: "/" },
         { name: "TV Shows", link: "/tv" },
         { name: "Movies", link: "/movies" },
         { name: "My List", link: "/mylist" },
       ];
+    let navigate = useNavigate();
+
+    onAuthStateChanged(firebaseAuth, (currentUser)=>{
+        if (!currentUser) {
+            navigate("/signup");
+            console.log("success");
+        }
+    })
     
+    let redirectToLink = (link)=>{
+        navigate(link);
+    }
 
     return(
-        <div className="bg-black text-white flex items-center justify-between px-6 py-4">
+        
+        <div className="bg-black text-gray-100 flex items-center justify-between px-6 py-4">
             <div className="flex items-center justify-between">
                 <div>
                     <img className="h-12" src="/src/assets/logo.png" alt="Logo"/>
@@ -18,7 +33,7 @@ export default function Navbar({isScrolled}){
                 <div>
                     <ul className="flex space-x-4 ml-4">
                         {links.map((link) => (
-                            <li key={link.name} className="hover:underline cursor-pointer">{link.name}</li>
+                            <li key={link.name} className="hover:underline cursor-pointer" onClick={()=>redirectToLink(link.link)}>{link.name}</li>
                         ))}
                     </ul>
                 </div>
@@ -33,6 +48,7 @@ export default function Navbar({isScrolled}){
                 </div>
                 <div>
                     <button
+                        onClick={() => signOut(firebaseAuth)}
                         className="text-white font-semibold bg-red-600 py-2 px-4 rounded-md hover:bg-red-700 transition duration-300"
                     >
                         Sign out
--- netflix-frontend/src/Pages/Netflix.jsx
+++ netflix-frontend/src/Pages/Netflix.jsx
@@ -1,17 +1,33 @@
 import { useState } from "react";
 import Navbar from "../Components/Navbar";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
 
 export default function Netflix(){
-    let [isScrolled, setIsScrolled] = useState(false);
 
-    window.onscroll = ()=>{
-        setIsScrolled((window.scrollY === 0)? false: true);
-        return () => (window.onscroll = null);
-    }
 
     return(
-        <div>
-            <Navbar isScrolled={isScrolled}/>
+        <div className="bg-black h-screen w-screen">
+    <Navbar />
+    <div className="bg-inherit relative">
+        <img src="src/assets/home.jpg" className="object-cover h-full w-full" />
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div id="movieInfo" className="absolute left-0 bottom-20 p-6 text-white flex flex-col space-y-4 ">
+            <div className="h-3/4">
+                <img src="src/assets/true.webp" className="h-full object-contain" />
+            </div>
+            <div className="flex p-2">
+                <button className="flex items-center ml-4 bg-white text-black px-8 py-2 rounded-md hover:bg-gray-300 transition duration-300">
+                    <PlayArrowIcon /> <span>Play</span>
+                </button>
+                <button className="flex items-center ml-4 bg-gray-700 text-white px-8 py-2 rounded-md hover:bg-gray-600 transition duration-300">
+                    <InfoOutlinedIcon /> <span>More info</span>
+                </button>
+            </div>
         </div>
+    </div>
+</div>
+
+
     );
 }
