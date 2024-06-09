ed6d1715f36af67427906cd3befb888338818f50
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2024-06-09 19:02:14 +0530

    Changes made to Card.jsx
    Changed Card.jsx to import references to images for deployment ease.

--- netflix-frontend/src/Components/Card.jsx
+++ netflix-frontend/src/Components/Card.jsx
@@ -1,13 +1,29 @@
 import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
+// Dynamically import all images from the assets folder
+const images = import.meta.glob('../assets/**/*.{png,jpg,jpeg,gif,jpeg.webp,webp}');
 
 export default function Card({ item }) {
     const navigate = useNavigate();
+    const [imageSrc, setImageSrc] = useState(null);
+
+    useEffect(() => {
+        const loadImage = async () => {
+            const path = item.image.replace('src/assets/', '../assets/');
+            const imageModule = await images[path]();
+            setImageSrc(imageModule.default);
+        };
+
+        loadImage();
+    }, [item.image]);
 
     return (
         <div 
         onClick={() => navigate("/details", { state: { item } })}
         className="min-w-80 ml-2 p-5 rounded overflow-hidden shadow-lg bg-inherit transition-transform hover:shadow-red-800/80 hover:scale-105">
-            <img className="m-2 max-h-96 min-h-96 w-64 object-cover" src={item.image} alt={item.name} />
+            {imageSrc && <img   className="m-2 max-h-96 min-h-96 w-64 object-cover"
+                                src={imageSrc} alt={item.name} />}
             <div className="p-6">
                 <h1 className="font-bold text-md mb-2">{item.name}</h1>
             </div>
